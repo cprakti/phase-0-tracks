d51464b4502e7d2ebfdd0e32a676dd61b1a03ac8
Chris Praktish <chris.praktish@gmail.com>
2016-03-21 17:55:22 -0400

    Add vampire related questions

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,2 +1,14 @@
-puts "True or false - you are a vampire."
-vampire = gets.chomp
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "In what year were you born?"
+born = gets.chomp
+
+puts "Our cafeteria makes some fantastic garlic bread.  Would you like me to order you some?"
+garlic = gets.chomp
+
+puts "Would you like to enroll in the company health insurance plan?"
+insurance = gets.chomp
