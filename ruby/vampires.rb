4ef2fa9db1a69bba06c200583b5b380e3723841b
Chris Praktish <chris.praktish@gmail.com>
2016-03-21 18:27:52 -0400

    Add first logic step

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,14 +1,36 @@
 puts "What is your name?"
 name = gets.chomp
 
-puts "How old are you?"
-age = gets.chomp
+puts "How old will you be on Dec 31st, 2016?"
+age = gets.chomp.to_i
+puts age
 
 puts "In what year were you born?"
-born = gets.chomp
+year = gets.chomp.to_i
+num = 2016 - year
+puts num
 
 puts "Our cafeteria makes some fantastic garlic bread.  Would you like me to order you some?"
-garlic = gets.chomp
+  answer = gets.chomp
+  if answer == "yes"
+    garlic = true
+  else
+    garlic = false
+  end
+puts garlic
 
 puts "Would you like to enroll in the company health insurance plan?"
-insurance = gets.chomp
+  ins_answer = gets.chomp
+  if ins_answer == "yes"
+    insurance = true
+  else
+    insurance = false
+  end
+puts insurance
+
+
+if (age == num) && (garlic == true)
+  puts "Probably not a vampire."
+else
+  puts "Guards!"
+end
