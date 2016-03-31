bb5b363ed14867498d6e4ea9e532abf829ffe12b
Chris Praktish <chris.praktish@gmail.com>
2016-03-31 13:25:42 -0400

    iteration.rb

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,1 +1,8 @@
 # Pair Session
+
+def block_method (bar1, bar2, bar3)
+  yield(bar1, bar2, bar3)
+  puts "The block is finished running."
+end
+
+block_method(1, 2, 5) {|x, y, z| puts "#{x + y + z}"}
