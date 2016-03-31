1659abc6f23cadaddd71f2012886bdf0fb16ea19
Chris Praktish <chris.praktish@gmail.com>
2016-03-31 15:55:56 -0400

    Add nested data structure

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,1 +1,75 @@
-qq
+apartment_building = {
+  first_floor: {
+    apartment_1a: {
+      family: "Smith",
+      num_residents: 3,
+      names: [
+        "Kathy",
+        "Tommy",
+        "Erica"
+      ]
+      pets: 2
+    },
+    apartment_1b: {
+      family: "Johnson",
+      num_residents: 5,
+      names: [
+        "Robert",
+        "Amy",
+        "Sharod",
+        "Emily",
+        "Chris"
+      ]
+      pets: 0
+    }
+    },
+  },
+  second_floor: {
+    apartment_2a: {
+      family: "Brown",
+      num_residents: 2,
+      names: [
+        "Kelly",
+        "Austin"
+      ]
+      pets: 4
+    }
+    },
+    apartment_2b: {
+      family: "Trump",
+      num_residents: 1,
+      names: [
+        "Donald"
+      ]
+      pets: 1
+    }
+    },
+  },
+  third_floor: {
+    apartment_3a: {
+      family: "Clinton",
+      num_residents: 3,
+      names: [
+        "Bill",
+        "Hillary",
+        "Guest"
+      ]
+      pets: 3
+    }
+    },
+    apartment_3b: {
+      family: "Bernie",
+      num_residents: 7,
+      names: [
+        "Bernie",
+        "Ernie",
+        "Marney",
+        "Gerney",
+        "Journey",
+        "Ted"
+      ]
+      pets: 20
+    }
+    }
+  }
+}
