8d585c9a6a1ee782a8f8584bdad078ef6fb091cd
Chris Praktish <chris.praktish@gmail.com>
2016-03-31 16:36:25 -0400

    Add first and last prompts and test

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,17 @@
+# Ask spy real's first name
+puts "What is your first name?"
+# Store spy's real first name
+first = gets.chomp.downcase
+# Ask spy real's last name
+puts "What is your last name?"
+# Store spy's real last name
+last = gets.chomp.downcase
+
+# Test
+puts first
+puts last
+
+# Swap first name and last name
+
+# Change vowels to next vowel (aeiou)
+# Change consonants to next consonant
