4b08146de1875f1e31ac14c36c0e4d766ab13a5f
Chris Praktish <chris.praktish@gmail.com>
2016-04-01 14:31:45 -0400

    Create loop for release 1

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,21 +1,29 @@
-# Ask spy real's first name
-puts "What is your first name?"
-# Store spy's real first name
-first = gets.chomp.downcase
-# Ask spy real's last name
-puts "What is your last name?"
-# Store spy's real last name
-last = gets.chomp.downcase
-# Test
-#puts "Your name is #{first} #{last}."
-# Swap first name and last name
-first, last = last, first
-# Test
-#puts "Your scrambled name is #{first} #{last}."
-# Change vowels to next vowel (aeiou)
-first_array = first.split('').to_a
-# Test
-# p first_array
-p first_array.select { |letter| letter =~ /[aeiou]/ }
+puts "Welcome to the alias creator. Make as many aliases as you would like, and when you are finished type 'exit'.\nPress any key to continue."
+gets
+# Test p user_resp = ''
+until user_resp == 'exit' # Loop until user types exit
+  puts "What is your first name?" # Ask spy real's first name
+  first = gets.chomp.downcase # Store spy's real first name
+  puts "What is your last name?" # Ask spy real's last name
+  last = gets.chomp.downcase # Store spy's real last name
+  # Test puts "Your name is #{first} #{last}."
 
-# Change consonants to next consonant
+  first, last = last, first # Swap first name and last name
+  # Test puts "Your scrambled name is #{first} #{last}."
+  first_array = first.split('').to_a # Change string to array
+  last_array = last.split('').to_a # Change string to array
+  # Test p first_array
+
+  first_array.map! { |letter| letter.tr('aeiou', 'eioua') } # Change vowels to next vowel (aeiou)
+  last_array.map! { |letter| letter.tr('aeiou', 'eioua') } # Change vowels to next vowel (aeiou)
+  first_array.map! { |letter| letter.tr('bcdfghjklmnpqrstvwxyz', 'cdfghjklmnpqrstvwxyzb')} # Change consonants to next consonant
+  last_array.map! { |letter| letter.tr('bcdfghjklmnpqrstvwxyz', 'cdfghjklmnpqrstvwxyzb')} # Change consonants to next consonant
+
+  alias_first = first_array.join # Join arrays back to string
+  alias_last = last_array.join # Join arrays back to string
+  alias_full = alias_first.capitalize + " " + alias_last.capitalize # Create capitalized version of full alias
+
+  puts "Your new alias is #{alias_full}"
+  puts "Would you like to make a new alias?"
+  # Test p user_resp = gets.chomp.downcase
+end
