dd73dcd9fad5ec0d93b8776e16707b7add51f116
Chris Praktish <chris.praktish@gmail.com>
2016-04-03 22:54:04 -0400

    Question 4

--- ruby/calculator.rb
+++ ruby/calculator.rb
@@ -7,11 +7,15 @@ end
 # calculate(2, '*', 4)
 # calculate(2, '/', 4)
 
-puts "What calculation would you like to perform?"
-user_calc = gets.chomp
+user_calc = ''
+until user_calc == "done"
+  puts "What calculation would you like to perform?"
+  user_calc = gets.chomp.downcase
+  unless user_calc == "done"
+    user_calc.split(' ')
+    first_num = user_calc[0].to_i
+    second_num = user_calc[4].to_i
+    calculate(first_num, user_calc[2], second_num)
+  end
+end
 
-user_calc.split(' ')
-first_num = user_calc[0].to_i
-second_num = user_calc[4].to_i
-
-calculate(first_num, user_calc[2], second_num)
