6829d335d742988dd4b9ac2998a0cffd4a01b9a0
Chris Praktish <chris.praktish@gmail.com>
2016-04-03 23:10:17 -0400

    Question 5

--- ruby/calculator.rb
+++ ruby/calculator.rb
@@ -1,5 +1,5 @@
 def calculate(x, op, y)
-  p x.send(op, y)
+  p answer = x.send(op, y)
 end
 
 # calculate(2, '+', 4)
@@ -7,6 +7,7 @@ end
 # calculate(2, '*', 4)
 # calculate(2, '/', 4)
 
+calc_hash = {}
 user_calc = ''
 until user_calc == "done"
   puts "What calculation would you like to perform?"
@@ -15,7 +16,10 @@ until user_calc == "done"
     user_calc.split(' ')
     first_num = user_calc[0].to_i
     second_num = user_calc[4].to_i
-    calculate(first_num, user_calc[2], second_num)
+    answer = calculate(first_num, user_calc[2], second_num)
+    calc_hash[user_calc] = answer
   end
 end
 
+puts "You performed #{calc_hash.length} calculations."
+calc_hash.each {|user_calc, answer| puts "#{user_calc} = #{answer}" }
