544eb3f8fe36a26b28094ff2d4517563f7d42684
Chris Praktish <chris.praktish@gmail.com>
2016-04-04 14:47:15 -0400

    Release 0

---
+++ ruby/puppy.rb
@@ -0,0 +1,17 @@
+# Breed -----------------------------------------
+# Mother's breed:
+# Father's breed:
+
+# Characteristics -----------------------------
+# Name:
+# Age:
+# Color:
+# Long or short hair?
+# Litter size?
+
+# Behavior ------------------------------------
+# Puppy eyes
+# Chew
+# Bark
+# Roll
+# Pee
