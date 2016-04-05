f4e172a53b066794e12ff453c6d7c9027f4cb509
Chris Praktish <chris.praktish@gmail.com>
2016-04-05 11:35:37 -0400

    Release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,30 @@ class Puppy
     toy
   end
 
+  def speak(x)
+    x.times do
+      puts "Woof!"
+    end
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(y)
+    p y * 7
+  end
+
+  def paw
+    puts "Doggie high-five!"
+  end
+
 end
 
+fluffy = Puppy.new
+
+fluffy.fetch("toy")
+fluffy.speak(2)
+fluffy.roll_over
+fluffy.dog_years(3)
+fluffy.paw
