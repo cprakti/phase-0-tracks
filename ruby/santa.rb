0c856f1cb3a7d4440d3d1605744acac0354a4564
Chris Praktish <chris.praktish@gmail.com>
2016-04-05 10:53:27 -0400

    Build santa data structure

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,15 +1,25 @@
 class Santa
+  def initialize(gender, ethnicity)
+    puts "Initializing Santa instance..."
+    @gender = gender
+    @ethnicity = ethnicity
+  end
   def speak
     puts "Ho, ho, ho! Haaaappy Hollidays!"
   end
   def eat_milk_and_cookies(cookie)
     puts "That was a good #{cookie} cookie!"
   end
-  def initialize
-    puts "Initializing Santa instance..."
-  end
+  @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+  @age = 0
+  puts "This santa identifies as #{gender} and #{ethnicity}."
 end
 
-santa1 = Santa.new
-santa1.speak
-santa1.eat_milk_and_cookies("sugar")
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
