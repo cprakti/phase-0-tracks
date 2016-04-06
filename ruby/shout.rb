1c6c2ee38cf87f290fa1f10c55bfe87eef0c611b
Chris Praktish <chris.praktish@gmail.com>
2016-04-06 12:33:12 -0400

    Release 0 build module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,14 @@
 module Shout
-  # Sample
+  def self.yell_angrily(words)
+    puts words + "!!!" + " :("
+  end
+
+  def self.yell_happily(words)
+    puts words + "!" + " :)"
+  end
 end
+
+
+Shout.yell_angrily("Kaiser Permanente")
+
+Shout.yell_happily("Huzzah")
