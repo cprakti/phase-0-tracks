7f644e471332b284cb29705e2cca9c1842a2ce6f
Chris Praktish <chris.praktish@gmail.com>
2016-04-11 11:28:35 -0400

    Questions 3 and 4

--- practice_test/dance.rb
+++ practice_test/dance.rb
@@ -6,6 +6,7 @@ class Dancer
   def initialize(name, age)
     @name = name
     @age = age
+    @card = []
   end
 
   def pirouette
@@ -16,12 +17,38 @@ class Dancer
     return "*bows*"
   end
 
+  def queue_dance_with(partner)
+    @partner = partner
+    @card.push @partner
+  end
+
+  def card
+    @card
+  end
+
+  def begin_next_dance
+    return "Now dancing with #{@card.slice!(0)}."
+  end
+
+#Create a method to make dancer breakdance.
+  def breakdance
+    return "*breakdances*"
+  end
+
 end
 
 dancer = Dancer.new("Misty Copeland", 33)
-p dancer.name
-p dancer.age
-p dancer.age = 34
-p dancer.age
-p dancer.pirouette
-p dancer.bow
+
+# p dancer.name
+# p dancer.age
+# p dancer.age = 34
+# p dancer.age
+# p dancer.pirouette
+# p dancer.bow
+
+# dancer.queue_dance_with("Sammy")
+# p dancer.card
+# dancer.queue_dance_with("Boris")
+# p dancer.card
+# dancer.begin_next_dance
+# p dancer.card
--- practice_test/dance_spec.rb
+++ practice_test/dance_spec.rb
@@ -24,4 +24,23 @@ describe Dancer do
     expect(dancer.bow).to eq "*bows*"
   end
 
+  it "can queue parters on the dance card" do
+    dancer.queue_dance_with("Mikhail Baryshnikov")
+    expect(dancer.card).to eq ["Mikhail Baryshnikov"]
+    dancer.queue_dance_with("Anna Pavlova")
+    expect(dancer.card).to eq ["Mikhail Baryshnikov", "Anna Pavlova"]
+  end
+
+  it "can start the next dance in the queue" do
+    dancer.queue_dance_with("Mikhail Baryshnikov")
+    dancer.queue_dance_with("Anna Pavlova")
+    expect(dancer.begin_next_dance).to eq "Now dancing with Mikhail Baryshnikov."
+    expect(dancer.card).to eq ["Anna Pavlova"]
+  end
+
+#Create test to check functionality of breakdance method.
+  it "starts breakdancing" do
+    expect(dancer.breakdance).to eq "*breakdances*"
+  end
+
 end
