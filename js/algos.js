b865a35d27849ba90ebafe3e9279240268866f17
Chris Praktish <chris.praktish@gmail.com>
2016-04-18 01:22:06 -0400

    Release 0 build program that return largest string

--- js/algos.js
+++ js/algos.js
@@ -1,19 +1,26 @@
 // Create variable that contains an array of strings
 // Create function that accepts an array as a parameter
-// Inside the function loop through array and find length of each string
+// Inside the function, loop through array and find length of each string
 // Store string as key and length as value in object
 // Sort keys by largest value from largest to smallest
 // Return first key
 
 var exampleArray = ["longest phrase", "long phrase", "longer phrase"];
-var lengths = {};
+var largestNum = 0;
+var longestStrings = [];
 function longestItem(exampleArray) {
   for ( var i = 0; i < exampleArray.length; i++ ){
-
-    lengths[exampleArray[i].length] = exampleArray[i];
-  };
-  console.log(lengths.valueOf(-1));
+    if (exampleArray[i].length > largestNum) {
+      largestNum = exampleArray[i].length;
+    }
+  }
+  for ( var i = 0; i < exampleArray.length; i++ ){
+    if (exampleArray[i].length == largestNum) {
+      longestStrings += (exampleArray[i]) + " ";
+    }
+  }
+  return longestStrings
 };
 
 
-longestItem(exampleArray);
+console.log(longestItem(exampleArray));
